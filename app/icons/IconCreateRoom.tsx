20d7c1260b0b543ca9d241e6c7fbe6b6c5a80a88
lrs2187 <liurongshuo2022@outlook.com>
2025-05-25 14:38:30 +0800

    💫feat: 添加导航栏图标

---
+++ app/icons/IconCreateRoom.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function IconCreateRoom({ size = 16 }: { size?: number | string }) {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" width={size} height={size} viewBox="0 0 16 16">
+            <path d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H4.414A2 2 0 0 0 3 11.586l-2 2V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12.793a.5.5 0 0 0 .854.353l2.853-2.853A1 1 0 0 1 4.414 12H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z" />
+            <path
+                transform="translate(2, 0) scale(0.75)"
+                d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"
+            />
+        </svg>
+    );
+}
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -3,36 +3,57 @@ import { Button, Frame } from '@/wm-ui';
 import Translator from '~/translation/translator';
 import type { Route } from './+types/index';
 import ToggleThemeButton from '~/components/ToggleThemeButton';
+import IconSettings from '~/icons/IconSettings';
+import IconFriend from '~/icons/IconFriend';
+import IconChat from '~/icons/IconChat';
+import IconAddFriend from '~/icons/IconAddFriend';
+import IconCreateRoom from '~/icons/IconCreateRoom';
 
 export default function Index() {
     const tr = Translator('index', 'zh');
 
     return (
-        <div className="flex flex-col h-full p-2 gap-2">
+        <div className="flex h-full flex-col gap-2 bg-slate-100 p-2 dark:bg-slate-800">
             <div className="flex gap-2">
                 <div>水瓜聊天</div>
                 <div className="flex-1">{/* 幽灵 div 实现中间留白 */}</div>
                 <div>欢迎您，******</div>
             </div>
-            <div className="flex-1 flex gap-2">
-                <div className="flex flex-col gap-2" style={{ width: 45 }}>
-                    <Button>feat</Button>
-                    <Button>feat</Button>
-                    <Button>feat</Button>
-                    <Button>feat</Button>
+            <div className="flex flex-1 gap-2">
+                <div className="flex flex-col gap-2" style={{ width: 44 }}>
+                    <Button className="py-2">
+                        <IconChat size="100%" />
+                    </Button>
+                    <Button className="py-2">
+                        <IconFriend size="100%" />
+                    </Button>
+                    <Button className="py-2">
+                        <IconAddFriend size="100%" />
+                    </Button>
+                    <Button className="py-2">
+                        <IconCreateRoom size="100%" />
+                    </Button>
 
                     <div className="flex-1">{/* 幽灵 div 实现中间留白 */}</div>
-                    <ToggleThemeButton />
-                    <Button>feat</Button>
+                    <ToggleThemeButton className="text-2xl" radius={22} />
+                    <Button className="py-2">
+                        <IconSettings size="100%" />
+                    </Button>
                 </div>
-                <Frame className="flex flex-col" style={{ width: 150 }}>
-                    Middle
+                <Frame className="flex flex-col bg-white dark:bg-gray-950" style={{ width: 150 }}>
+                    <h1 className="p-1 text-2xl">Title</h1>
                     <hr />
-                    <div className="flex-1">List</div>
+                    <div className="flex flex-1 flex-col gap-1 p-1">
+                        <Frame className="p-1">朋友1号</Frame>
+                        <Frame className="p-1">朋友2号</Frame>
+                        <Frame className="p-1">朋友3号</Frame>
+                    </div>
                 </Frame>
-                <div className="flex-1">
-                    <Frame className="h-full">
-                        欢迎来到 WaterChat
+                <div className="flex-1 bg-white dark:bg-gray-950">
+                    <Frame className="flex h-full flex-col">
+                        <div className="m-auto text-center text-2xl text-gray-300 dark:text-gray-700">
+                            欢迎来到 WaterChat
+                        </div>
                     </Frame>
                 </div>
             </div>
